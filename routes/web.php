033158ba8b71be9d5b1d68bf5e543a1ab39ef70e
qianyewoailuo <qianyewoailuo@126.com>
2019-06-29 15:53:23 +0000

    话题基本骨架生成

--- routes/web.php
+++ routes/web.php
@@ -51,3 +51,5 @@ Route::resource('users', 'UsersController',['only'=>['show','update','edit']]);
 // Route::get('/users/{user}','UsersController@show')->name('users.show');
 // Route::get('/users/{user}/edit','UsersController@edit')->name('users.edit');
 // Route::patch('/users/{user}','UsersController@update')->name('users.update');   // 这里的patch也可以用put代替
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
