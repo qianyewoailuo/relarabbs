4be13a90d07c5350f90fe430b86102eeaefa087a
qianyewoailuo <qianyewoailuo@126.com>
2019-06-30 13:32:50 +0000

    用户假数据填充

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@
 use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +18,15 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $faker_time = $faker->date.''.$faker->time;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make('luo12345'), // 或者使用 bcrypt() 哈希密码辅助函数
         'remember_token' => Str::random(10),
+        'introduction' => $faker->sentence(), // 小段落文本
+        'created_at' => $faker_time,
+        'updated_at' => $faker_time,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
+            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+        ];
+
+        // 生成数据集合
+        $users = factory(User::class) // 根据指定的 User 生成模型工厂构造器，对应加载 UserFactory.php 中的工厂设置。
+                    ->times(10) // 生成数据的数量
+                    ->make()    // 生成集合对象
+                    ->each(function ($user,$index) use ($faker,$avatars){
+                        // each是 合对象提供的方法，用来迭代集合中的内容并将其传递到回调函数中
+                        // 从头像数组中随机取出一个赋值
+                        $user->avatar = $faker->randomElement($avatars);  // 随机元素可以使用 array_rand 或 mt_rand 等函数综合获取
+                    });
+
+        // 让隐藏字段可见并将数据集合转换为数组,确保入库时数据库不会报错
+        $user_array = $users->makeVisible([
+            'password','remember_token'
+        ])->toArray();
+
+        // 插入数据
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'qianyewoailuo';
+        $user->email = 'qianyewoailuo@126.com';
+        $user->avatar = 'https://s2.ax1x.com/2019/05/05/E04Mb6.jpg';
+        $user->save();
+    }
+}
