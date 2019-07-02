01b685e675797c6027be5a8465b4c7ac711331a2
qianyewoailuo <qianyewoailuo@126.com>
2019-07-02 13:29:01 +0000

    HTMLPurifier过滤XXS攻击

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,8 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        // HTMLPurifier 白名单XXS过滤
+        $topic->body = clean($topic->body, 'user_topic_body');
         // make_excerpt 是自定义的辅助函数.用来生成摘要
         $topic->excerpt = make_excerpt($topic->body);
     }
