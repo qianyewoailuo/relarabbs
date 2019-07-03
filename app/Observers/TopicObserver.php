1c4b748e70b269f4d8c7c6fc46e4ccb40b4e9099
qianyewoailuo <qianyewoailuo@126.com>
2019-07-03 13:03:07 +0000

    队列与redis异步队列

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -19,9 +20,17 @@ class TopicObserver
         $topic->excerpt = make_excerpt($topic->body);
 
         // slug SEO优化字段生成
-        if(! $topic->slug){
-            // 如果slug字段无内容则进行翻译生成
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        // if(! $topic->slug){
+        //     // 如果slug字段无内容则进行翻译生成 - 非队列
+        //     $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        // }
+    }
+
+    public function saved(Topic $topic)
+    {
+        if (!$topic->slug) {
+            // 使用队列进行 slug 翻译
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
